40ea0a200cd9b55c6ab4dcc1eff8f8649d13067f
chenzhichong <151698294@qq.com>
2013-08-30 15:34:05 +0800

    Update helloworld.java
    edit at github

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -7,6 +7,7 @@ public class helloworld {
 		System.out.println("hello world!!!");
 		System.out.println("hello world!!! angain!");
 		System.out.println("hhhhhhhhh");
+		System.out.println("edit at github");
 	}
 
 }
